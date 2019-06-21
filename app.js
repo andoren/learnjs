3921d056aa35a891d75093bf4df474d4b468bc77
andoren <mpekar55@gmail.com>
2019-06-21 20:16:47 +0200

    Console practice

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Kiscica");
+//mennyi ideig tart a script (a console.time és a console.timeEnd ugyan annak kell lennie a stringnek azaz a labelnek akkor az állítja le)
+console.time('Hello');
+console.log("Hello world in console");
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log(['Valmai','Tömbben']);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+//console.error("This is an error");
+//console.clear();
+//console.warn('This is a warning');
+console.timeEnd('Hello');
